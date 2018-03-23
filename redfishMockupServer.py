e6897f1cdecd72c6f64e4f37482b20c31e3bf6b9
Mike Raineri <michael.raineri@dell.com>
2018-03-23 09:44:14 -0400

    Corrected link to license in source files

--- redfishMockupServer.py
+++ redfishMockupServer.py
@@ -1,6 +1,6 @@
 # Copyright Notice:
 # Copyright 2016 Distributed Management Task Force, Inc. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Mockup-Server/LICENSE.md
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Mockup-Server/blob/master/LICENSE.md
 
 # redfishMockupServer.py
 # tested and developed Python 3.4
